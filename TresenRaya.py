7eb2517bd3ce3422b1924a30c386e9e4f6e660bb
IngDiazKevin <112918282+IngDiazKevin@users.noreply.github.com>
2024-05-31 23:41:56 -0400

    TresenRaya.py

--- TresenRaya.py
+++ TresenRaya.py
@@ -1,7 +1,4 @@
-#Dibujar el tablero 
-
-tablero = [['1','2','3'],['4','5','6'],['7','8','9']]
-#print(tablero[0][2])
+from random import randrange
 
 #--1) Mostrar el tablero
 #Crear una funcion que permita mostrar el tablero
@@ -81,12 +78,107 @@ def CrearListaConEspaciosVacios(tablero):
             if tablero[fila][columna] == 'X' or tablero[fila][columna] == 'O':
                 pass
             else:
-               espacios_free.append(([fila,columna]))
+               
     
+               espacios_free.append(([fila,columna]))
     print(espacios_free)
+    return espacios_free
         
 
+#---4
+#Funcion para verificar la victoria
+
+def VerificarVictoria(tablero, signo):
+    if signo == 'O':
+        print('Verificar si TU ganaste')
+    else:
+        print('Verificar si la PC ganó')
+        
+    for i in range(3):
+        if tablero[i][0] == signo and tablero[i][1] == signo and tablero [i][2] == signo:
+            return True
+        if tablero[0][i] == signo and tablero[1][i] == signo and tablero [2][i] == signo:
+            return True
+        
+    if tablero [0][0] == signo and tablero [1][1] == signo and tablero[2][2] == signo:
+        return True
+    if tablero [0][2] == signo and tablero[1][1] == signo and tablero[2][0] == signo:
+        return True
+    
+    return False
+
+#--5
+#Funcion para el movimiento de la maquina
+
+def MovimientoMaquina(tablero):
+    while True:
+        fila = randrange(3)
+        columna = randrange(3)
+        
+        print('num ale',fila, columna)
+        
+        if (fila, columna) not in CrearListaConEspaciosVacios(tablero):
+            #tablero[fila, columna] = 'X'
+            continue
+        else:
+            tablero[fila][columna] = 'X'
+            #break
+            return
+            #continue
+
+#Codigo principal del juego
+#Dibujar el tablero 
+tablero = [['1','2','3'],['4','X','6'],['7','8','9']]
+humano = 'O' 
+computador = 'X'
+numero_movimientos = 1
+
+print('---- BIENVENIDO AL JUEGO ----')
+print('')
+print('')
+print('Estado actual del tablero')
 MostrarTablero(tablero)
-IngresarMoviento(tablero)
-MostrarTablero(tablero)
-CrearListaConEspaciosVacios(tablero)
+print('')
+
+while numero_movimientos < 9:
+    IngresarMoviento(tablero)
+    numero_movimientos = numero_movimientos + 1
+    MostrarTablero(tablero)
+    
+    if VerificarVictoria(tablero, humano):
+        print('GANASTE!')
+        break
+    else:
+        print('Espacios vacios del tablero')
+        CrearListaConEspaciosVacios(tablero)
+      
+    print('')    
+    print('AHORA ES EL TURNO DE LA PC')
+    MovimientoMaquina(tablero)
+    numero_movimientos = numero_movimientos + 1
+    MostrarTablero(tablero)
+    print('')
+    
+    if VerificarVictoria(tablero, computador):
+        print('LA PC HA GANADO! perdiste')
+        break
+    else:
+        print('Espacios vacios del tablero')
+        CrearListaConEspaciosVacios(tablero)
+        MostrarTablero(tablero)
+    
+else:
+    print('EMPATE')
+    
+print('gracias por jugar') 
+
+        
+    
+    
+    
+
+
+#MostrarTablero(tablero)
+#IngresarMoviento(tablero)
+#MostrarTablero(tablero)
+#CrearListaConEspaciosVacios(tablero)
